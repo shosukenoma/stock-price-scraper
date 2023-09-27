d83d3783b9c3ec06c2cf28831380be9d58545472
Shosuke Noma <47355058+shosukenoma@users.noreply.github.com>
2023-09-26 21:42:17 -0500

    Re-integrate CSV writing and resolve bottleneck

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"log"
+	"os"
+
+	// "os"
 
 	"github.com/gocolly/colly"
 )
@@ -47,15 +52,15 @@ func main() {
 		fmt.Println("Visiting:", r.URL)
 	})
 
-	// c.OnError(func(_ *colly.Response, err error) { 
-	// 	log.Println("Something went wrong: ", err) 
-	// }) 
+	c.OnError(func(_ *colly.Response, err error) { 
+		log.Println("Something went wrong: ", err) 
+	}) 
 	 
 	// c.OnResponse(func(r *colly.Response) { 
 	// 	fmt.Println("Page visited: ", r.Request.URL) 
 	// }) 
 
-	// iterating over the list of HTML product elements
+	// Iterate over the list of HTML product elements
 	// e represents a single li.product element
 	c.OnHTML("div#quote-header-info", func(e *colly.HTMLElement) {
 		// initializing a new PokemonProduct instance
@@ -75,51 +80,52 @@ func main() {
 		stocks = append(stocks, stock)
 	})
 
+	// Resolved bottleneck section {writer.Write(record)}
+	// Helps program wait to write csv file until all scraping is done.
+	c.Wait()
+
 	for _, t := range ticker {
 		// c.Visit() has to come after all the callback functions
 		c.Visit("https://finance.yahoo.com/quote/" + t + "/")
 	}
 
 	fmt.Println(stocks)
-	
 
-	// // opening the CSV file 
-	// file, err := os.Create("products.csv") 
-	// if err != nil { 
-	// 	log.Fatalln("Failed to create output CSV file", err) 
-	// } 
-	// defer file.Close() 
+	// opening the CSV file 
+	file, err := os.Create("stocks.csv") 
+	if err != nil { 
+		log.Fatalln("Failed to create output CSV file", err) 
+	} 
+	defer file.Close() 
 
-	// // initializing a file writer 
-	// writer := csv.NewWriter(file) 
+	// initializing a file writer 
+	writer := csv.NewWriter(file) 
 	
-	// // defining the CSV headers 
-	// headers := []string{ 
-	// 	"url", 
-	// 	"image", 
-	// 	"name", 
-	// 	"price", 
-	// } 
-	// // writing the column headers 
-	// writer.Write(headers) 
+	// defining the CSV headers 
+	headers := []string{
+		"company", 
+		"price", 
+		"change", 
+	} 
+	// writing the column headers 
+	writer.Write(headers) 
 
-	// // adding each Pokemon product to the CSV output file 
-	// for _, pokemonProduct := range pokemonProducts { 
-	// 	// converting a PokemonProduct to an array of strings 
-	// 	record := []string{ 
-	// 		pokemonProduct.url, 
-	// 		pokemonProduct.image, 
-	// 		pokemonProduct.name, 
-	// 		pokemonProduct.price, 
-	// 	} 
+	// adding each stock to the CSV output file 
+	for _, stock := range stocks { 
+		// converting a stock to an array of strings 
+		record := []string{ 
+			stock.company,
+			stock.price,
+			stock.change,
+		} 
 	
-	// 	// writing a new CSV record 
-	// 	writer.Write(record) 
-	// } 
-	// // writer.Flush() ensures that any remaining data is written.
-	// // bufio.Writer sends data only when buffer is either full or when explicitly requested with Flush method.
-	// // https://medium.com/golangspec/introduction-to-bufio-package-in-golang-ad7d1877f762#:~:text=bufio.Writer%20sends%20data%20only%20when%20buffer%20is%20either%20full%20or%20when%20explicitly%20requested%20with%20Flush%20method.
-	// defer writer.Flush()
+		// writing a new CSV record 
+		writer.Write(record) // Bottleneck
+	} 
+	// writer.Flush() ensures that any remaining data is written.
+	// bufio.Writer sends data only when buffer is either full or when explicitly requested with Flush method.
+	// https://medium.com/golangspec/introduction-to-bufio-package-in-golang-ad7d1877f762#:~:text=bufio.Writer%20sends%20data%20only%20when%20buffer%20is%20either%20full%20or%20when%20explicitly%20requested%20with%20Flush%20method.
+	defer writer.Flush()
 }
 
 
